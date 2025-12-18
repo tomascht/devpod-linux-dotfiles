e81ca69bbde0ee96ae52fb2d3e857e4a1098e1ca
Tim Tomaschoff <tim.tomaschoff@berge-meer.de>
2025-12-18 10:51:27 +0100

    wip

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ echo "Installing dotfiles and more"
 #sudo apt install zsh
 
 #touch ~/.profile
-> ~/.profile
-echo "export SHELL=`which zsh`" >> ~/.profile
-echo "`[ -z "$ZSH_VERSION" ] && exec \"$SHELL\" -l" >> ~/.profile
+>~/.profile
+echo "export SHELL=$(which zsh)" >>~/.profile
+echo "[ -z "$ZSH_VERSION" ] && exec \"$SHELL\" -l" >>~/.profile
 
 echo "Install omz"
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
